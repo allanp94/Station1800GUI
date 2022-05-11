1d5a0f9665bac2e8ab04ab42167c100fea2e309a
allanp94 <allan.p94@gmail.com>
2022-05-11 15:58:49 -0500

    removed unitReady validation as it does not work on destination pc causing a redundant error; code works well in initial testing; need to validate that samples don't cause issues but only time will tell

---
+++ output/1800Script/Source code/Station1800.py
@@ -0,0 +1,35 @@
+from lib2to3.pgen2 import driver
+from MESintegration import MESLogIn
+from MESintegration import MESWork
+from ProcessKiller import killProcess
+
+# D1800
+# RFIDWritePlugin ---> files to put it in
+#LOGS/1800MESIntergrations ---> location of .txt file to read from
+#1800Script ---> .exe name
+
+dataFromFile = ['','','','','']
+
+#read data from .txt file 
+def openFile(name):
+    with open(name, "r") as file1800:
+        data = file1800.readlines()
+    for x, val in enumerate(data):
+        dataFromFile[x] = val.strip()
+
+#txt file location on deployment machine
+openFile(r"C:\LOGS\1800MESIntegration.txt")
+print(dataFromFile)
+
+# close other chrome browser(s) and driver(s)
+killProcess("CHROME.EXE")
+killProcess("chromedriverv80.exe")
+killProcess("chromedriver.exe")
+
+
+# pass in badge number to log in to MES
+# function returns the chromedriver instance
+driver = MESLogIn(dataFromFile[0])
+
+# pass in data and the driver that was used to sign in
+MESWork(dataFromFile, driver)
